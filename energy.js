1f3db4f37793d3fcc0e972f40c1d0c4f778caf1d
JackDunne <8jdunne@stpatrick.org>
2017-05-17 11:29:44 -0500

    Add files via upload

--- energy.js
+++ energy.js
@@ -6,3 +6,18 @@ let Ke = 0;
 Ke += m*v*v;
 Ke = Ke/2;
 return Ke;
+
+exports.mass = function(Ke,v){
+  let m = 0;
+  m += 2*Ke;
+  m = m/(v*v);
+  return m;
+};
+
+exports.velocity = function(Ke,m){
+  let v = 0;
+  v += 2*Ke;
+  v = v/m;
+  v = Math.sqrt(v);
+  return v;
+};
--- physics.js
+++ physics.js
@@ -9,7 +9,46 @@ const terminal = readline.createInterface({
 });
 
 terminal.question("What type of equation are you looking for?",function(str){
+if(str == "motion"){
+  terminal.question("What variable are you looking for?", function(str1){
+    if(str1 == "d"){
+      printOptions();
+    }
+    else if(str1 == "v"){
+      printOptions();
+    }
+    else if(str1 == "a"){
+      printOptions();
+    }
+    else if(str1 == "t"){
+      printOptions();
+    }
+    else{
+      console.log("Not a variable");
+    }
+  })
 
+  }
+  else if(str == "energy"){
+    terminal.question("What variable are you looking for?", function(str1){
+      if(str1 == "Ke"){
+        printOptions();
+      }
+      else if(str1 == "m"){
+        printOptions();
+      }
+      else if(str1 == "v"){
+        printOptions();
+      }
+      else{
+        console.log("Not a variable");
+      }
+    })
+  }
+  else{
+      console.log("That is not an option");
+    }
+}
     //you will configure your series of questions inside here.
 
 
